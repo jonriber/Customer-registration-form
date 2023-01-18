d6f0d9fd5a04c1dc4be1182304cb0935b7c82e87
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-01-17 22:25:53 -0300

    basic form style

--- customer-registration-form/src/App.tsx
+++ customer-registration-form/src/App.tsx
@@ -7,7 +7,36 @@ function App() {
 
   return (
     <div className="App">
-    
+      <div className='form'>
+        <h2>Registration</h2>
+        <br/>
+        <h3>Name:</h3>
+        <input type={"text"} placeholder='Name'/>
+        <br/>
+        <h3>Age:</h3>
+        <input type={"number"} placeholder='Age'/>
+        <br/>
+        <h3>CPF:</h3>
+        <input type={"number"} placeholder='CPF'/>
+        <br/>
+        <h3>Country:</h3>
+        <input type={"text"} placeholder='Country:'/>
+        <br/>
+        <h3>CEP:</h3>
+        <input type={"number"} placeholder='CEP:'/>
+        <br/>
+        <h3>Street:</h3>
+        <input type={"text"} placeholder='Street:'/>
+        <br/>
+        <h3>Number:</h3>
+        <input type={"number"} placeholder='Number:'/>
+        <br/>
+        <h3>Complement:</h3>
+        <input type={"text"} placeholder='Complement:'/>
+        <br/>
+        <h3>Email:</h3>
+        <input type={"email"} placeholder='Email:'/>
+      </div>
     </div>
   )
 }
---
+++ customer-registration-form/src/components/FormCard.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+
+
+const FormCard = (child:any) => {
+    return(
+        <Fragment>
+            {child}
+        </Fragment>
+    )
+};
+
+export default FormCard;
